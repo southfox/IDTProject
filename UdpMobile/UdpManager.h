a1dd73423291fd8eac3cf8452211a5b8bde2ca79
Javier Fuchs <Javier.X.Fuchs.-ND@disney.com>
2016-09-24 14:11:22 -0300

    Udp Manager

---
+++ UdpMobile/UdpManager.h
@@ -0,0 +1,15 @@
+//
+//  UdpManager.h
+//  IDTProject
+//
+//  Created by Javier Fuchs on 9/24/16.
+//  Copyright © 2016 Fuchs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UdpManager : NSObject
+
++ (UdpManager *)sharedInstance;
+
+@end
