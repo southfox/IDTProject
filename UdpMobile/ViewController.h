88f880bb6c9e7ba33b12650f2add0599ed1f63b1
Javier Fuchs <Javier.X.Fuchs.-ND@disney.com>
2016-09-24 14:00:20 -0300

    Adding Mobile side

---
+++ UdpMobile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UdpMobile
+//
+//  Created by Javier Fuchs on 9/24/16.
+//  Copyright © 2016 Fuchs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UdpMobile/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UdpMobile
+//
+//  Created by Javier Fuchs on 9/24/16.
+//  Copyright © 2016 Fuchs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
